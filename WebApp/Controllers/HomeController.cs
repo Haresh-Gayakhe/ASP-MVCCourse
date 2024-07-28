9c3218687d908c700546e347ae160727405a949b
Haresh Gayakhe <hgayakhe7@gmail.com>
2024-07-28 21:30:31 +0530

    added routing and handle requests with controllers

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello world from action method";
+        }
+
+        public string hello()
+        {
+            return "Hello world from another";
+        }
+    }
+}
